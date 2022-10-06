cc8c3ac864916c91a86a4bbf6aa347918c825dbc
Lukas Neubert <serkonda7@pm.me>
2022-10-06 17:13:31 +0200

    fix copy script

--- copy_dp_to_save.ps1
+++ copy_dp_to_save.ps1
@@ -1,13 +1,13 @@
-Set-StrictMode -Version 3.0
-
 # This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
 # If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
+Set-StrictMode -Version 3.0
+
 $MC_SAVES = "$($env:APPDATA)\.minecraft\saves"
 
 function Get-DatapackDestination([string] $savegame, [string] $dp) {
-	return "$MC_SAVES\$savegame\datapacks\$dp"
+	return "$savegame\datapacks\$dp"
 }
 
 $datapacks = Get-ChildItem -Path ".\datapacks" -Directory
@@ -24,8 +24,7 @@ for ($i = 0; $i -lt $savegames.Length; $i++) {
 $save_idx = Read-Host "Savegame index"
 $savegame = $savegames[$save_idx]
 
-$src = ".\datapacks\$dp_name"
 $dest = Get-DatapackDestination($savegame)
 
-Copy-Item $src $dest -Force -Recurse
+Copy-Item $dp_name $dest -Force -Recurse
 Write-Output "Copied $dp_name to $dest"
